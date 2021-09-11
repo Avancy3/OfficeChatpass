4b2030a994f14ce21029293eca15f053b424bebc
Avancy3 <89049717+Avancy3@users.noreply.github.com>
2021-09-11 12:22:26 +0900

    EarlyRelease

---
+++ OfficeCP1_ER.lua
@@ -0,0 +1,1 @@
+game.Players.LocalPlayer:Kick("Office Chatpass 1 Early Release is Not supported at this time. Please check the Latest Version of Chatpass for Use the Chatpass 2 / Chatpass 2 Early Release.")
